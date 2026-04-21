e1c46aa9926f08917f4724ed36243fe18673ff90
Ytzjak Diaz <ytzjakdawid1210@gmail.com>
2026-04-21 13:36:42 -0500

    Backend MercadoPago Vibewear

---
+++ server.js
@@ -0,0 +1,89 @@
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Servidor corriendo correctamente" });
+});
+app.get("/test-payment", async (req, res) => {
+    try {
+        const response = await fetch("https://api.mercadopago.com/checkout/preferences", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${ACCESS_TOKEN}`,
+            },
+            body: JSON.stringify({
+                items: [
+                    {
+                        title: "Producto de prueba",
+                        quantity: 1,
+                        currency_id: "COP",
+                        unit_price: 10000
+                    }
+                ],
+            }),
+        });
+
+        const data = await response.json();
+        res.send(`
+            <h1>Preferencia creada</h1>
+            <a href="${data.init_point}" target="_blank">Pagar con MercadoPago</a>
+        `);
+    } catch (error) {
+        console.error(error);
+        res.send("Error creando preferencia");
+    }
+});
+
+const ACCESS_TOKEN = "APP_USR-4721285442073566-041917-23754951322e0ac9a2364ef88e4eeab9-3346852400";
+
+app.post("/create-preference", async (req, res) => {
+    try {
+        const response = await fetch("https://api.mercadopago.com/checkout/preferences", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${ACCESS_TOKEN}`,
+            },
+            body: JSON.stringify({
+                items: req.body.items,
+                back_urls: {
+                    success: "http://localhost:5500/success.html",
+                    failure: "http://localhost:5500/failure.html",
+                    pending: "http://localhost:5500/pending.html",
+                },
+                auto_return: "approved",
+            }),
+        });
+
+const data = await response.json();
+
+console.log("Respuesta MercadoPago:");
+console.log(data);
+
+// Si MercadoPago devuelve error, lo enviamos al frontend
+        if (!data.init_point) {
+        return res.status(400).json({
+        error: "MercadoPago error",
+        details: data
+    });
+}
+
+        res.json({ init_point: data.init_point });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error creando preferencia" });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en http://localhost:3000");
+});
